d9119848c5e087dcf89b36c4f7c5012864354918
yattemiyou <71997636+yattemiyou@users.noreply.github.com>
2022-01-09 08:59:56 +0900

    Update main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,7 +1,12 @@
+#include <stdlib.h>
+#include <iostream>
+
 #include "Hello.h"
 
 int main(int argc, char *argv[])
 {
+    std::cout << system("gcc -v") << std::endl;
+
     Hello hello;
 
     hello.say();
